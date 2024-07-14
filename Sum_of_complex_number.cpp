5b9dc62d21caa23011fa8c2b580cae98b215f8cd
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2024-07-15 01:32:40 +0600

    operator overloading

---
+++ Sum_of_complex_number.cpp
@@ -0,0 +1,52 @@
+/*
+Write a class for complex number and calculate
+sum of 2 complex number using + operator overloading.
+*/
+
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+protected:
+    int real;
+    int img;
+
+public:
+    Complex() : real(0), img(0) {}
+    Complex(int r, int im) : real(r), img(im) {}
+
+    void display() const
+    {
+        cout << real << " + " << img << "i" << endl;
+    }
+
+    // Overload the + operator
+    friend Complex operator+(const Complex &c1, const Complex &c2);
+};
+
+Complex operator+(const Complex &c1, const Complex &c2)
+{
+    Complex temp;
+    temp.real = c1.real + c2.real;
+    temp.img = c1.img + c2.img;
+    return temp;
+}
+
+int main()
+{
+    cout << "Name: Md Shahajalal Mahmud" << endl
+         << "ID: 1539" << endl;
+
+    Complex C1(5, 4);
+    Complex C2(8, 2);
+    Complex C3;
+
+    C3 = C1 + C2;
+
+    C1.display();
+    C2.display();
+    C3.display();
+
+    return 0;
+}
