c94938fb45331068237b9297efcf2d9b17b6b384
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2024-07-15 01:17:30 +0600

    Added display matrix

--- 4_2_print_matrix.cpp
+++ 4_2_print_matrix.cpp
@@ -1,10 +1,12 @@
 /*
-4.1: Write a class and required function to read a matrix of
-size m*n from the keyboard with the help of constructor.
+Write a class and required function to read a matrix of size m*n
+from the keyboard using constructor and display the matrix.
 */
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
 class Matrix
 {
     int row;
@@ -43,19 +45,33 @@ public:
             }
         }
     }
+
+    void getMatrix() const
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                cout << setw(5) << matrix[i][j];
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
 };
 
 int main()
 {
     cout << "Name: Md Shahajalal Mahmud" << endl
          << "ID: 1539" << endl;
-         
+
     int m, n;
     cout << "Enter value for a matrix row and column (m, n): ";
     cin >> m >> n;
 
     Matrix M1(m, n);
-    M1.setMatrix();
 
+    M1.setMatrix();
+    M1.getMatrix();
     return 0;
 }
